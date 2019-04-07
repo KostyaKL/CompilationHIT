b95794960f039cfd25ab1f18cf4424769f3181b6
Lobby8 <Lobby8@LOBBY8-10>
2019-04-07 12:48:48 +0300

    lex file

--- test/test/main.c
+++ test/test/main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void main() {
-	printf("hello world\n");
-	system("pause");
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//void main() {
+//	printf("hello world\n");
+//	system("pause");
+//}
