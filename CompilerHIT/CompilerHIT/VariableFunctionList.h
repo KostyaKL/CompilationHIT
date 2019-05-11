3540c6d1a894304ad5bd6819f26202c86b714ef5
kostya <kostya@kl>
2019-05-11 13:18:55 +0300

    var list include

--- CompilerHIT/CompilerHIT/VariableFunctionList.h
+++ CompilerHIT/CompilerHIT/VariableFunctionList.h
@@ -1,6 +1,27 @@
 #ifndef VariableFunctionList_H
 #define VariableFunctionList_H
 
-#include "var_program.h"
+#include "parse_block.h"
+#include "parse_definitions.h"
+#include "parse_expression.h"
+#include "parse_expression_clean.h"
+#include "parse_func_definition.h"
+#include "parse_func_definitions.h"
+#include "parse_func_definitions_celan.h"
+#include "parse_function_call.h"
+#include "parse_param_definitions.h"
+#include "parse_parameters_list.h"
+#include "parse_program.h"
+#include "parse_returned_type.h"
+#include "parse_statment.h"
+#include "parse_statment_clean.h"
+#include "parse_statments.h"
+#include "parse_type.h"
+#include "parse_var_definition.h"
+#include "parse_var_definitions.h"
+#include "parse_variable.h"
+#include "parse_variable_clean.h"
+#include "parse_variable_list.h"
+#include "parse_variable_list_clean.h"
 
 #endif
