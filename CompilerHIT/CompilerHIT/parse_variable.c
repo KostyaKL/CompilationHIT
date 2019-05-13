7a32dcb6333cc766b51e26f427e74bd83f8a84b7
8lab102 <8lab102@8LAB102-17>
2019-05-13 11:53:14 +0300

    variable clean finished

--- CompilerHIT/CompilerHIT/parse_variable.c
+++ CompilerHIT/CompilerHIT/parse_variable.c
@@ -1,17 +1,19 @@
-#include "parse_variable.h"
+#include "VariableFunctionList.h"
 #include "Token.h"
+
+#define NUM_OF_EXPECTED 1
 
 void parse_variable() {
 	Token *cur_token = next_token();
+	eTOKENS expected[NUM_OF_EXPECTED] = { TOKEN_ID };
 	switch (cur_token->kind)
 	{
-	case TOKEN_EOF:
-		//print;
-		match(TOKEN_EOF);
+	case TOKEN_ID:
+		print_parser_rule("VARIABLE -> id VARIABLE_CLEAN");
+		parse_variable_clean();
 		break;
 	default:
-		//error;
+		error_recovery(VARIABLE, expected, NUM_OF_EXPECTED, cur_token);
 		break;
 	}
-
 }
--- CompilerHIT/CompilerHIT/parse_variable_clean.c
+++ CompilerHIT/CompilerHIT/parse_variable_clean.c
@@ -1,17 +1,23 @@
-#include "parse_variable_clean.h"
+#include "VariableFunctionList.h"
 #include "Token.h"
+
+#define NUM_OF_EXPECTED 2
 
 void parse_variable_clean() {
 	Token *cur_token = next_token();
+	eTOKENS expected[NUM_OF_EXPECTED] = { TOKEN_OPEN_SQUER_PAR, TOKEN_EOF };
 	switch (cur_token->kind)
 	{
+	case TOKEN_OPEN_SQUER_PAR:
+		print_parser_rule("VARIABLE_CLEAN -> [ int_number ]");
+		match(VARIABLE_CLEAN, TOKEN_INT_NUMBER);
+		match(VARIABLE_CLEAN, TOKEN_CLOSE_SQUER_PAR);
+		break;
 	case TOKEN_EOF:
-		//print;
-		match(TOKEN_EOF);
+		print_parser_rule("VARIABLE_CLEAN -> epsilon");
 		break;
 	default:
-		//error;
+		error_recovery(VARIABLE_CLEAN, expected, NUM_OF_EXPECTED, cur_token);
 		break;
 	}
-
 }
