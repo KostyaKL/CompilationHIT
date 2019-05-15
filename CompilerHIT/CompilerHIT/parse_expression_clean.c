79afadc53070ef10fac881a188a451f83e4ef35c
1lab500 <1lab500@1LAB500-04>
2019-05-15 15:40:50 +0300

    working!!!!

--- CompilerHIT/CompilerHIT/parse_expression_clean.c
+++ CompilerHIT/CompilerHIT/parse_expression_clean.c
@@ -22,10 +22,12 @@ void parse_expression_clean() {
 	case TOKEN_SEMICOLON:
 		print_parser_rule("EXPRESSION_CLEAN -> VARIABLE_CLEAN");
 		cur_token = back_token();
+		parse_variable_clean();
 		break;
 	case TOKEN_OPEN_SQUER_PAR:
 		print_parser_rule("EXPRESSION_CLEAN -> VARIABLE_CLEAN");
 		cur_token = back_token();
+		parse_variable_clean();
 		break;
 	default:
 		error_recovery(EXPRESSION_CLEAN, expected, NUM_OF_EXPECTED);
